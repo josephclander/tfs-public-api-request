ad19bda0db15ead30dfc1002e47d1a91ec5399ee
Joseph Lander <josephclander@gmail.com>
2021-03-22 16:42:10 +0000

    Add fetch to collect 12 responses, log to console

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log('Connected!');
+//
+// Fetch request
+//
+fetch(
+  'https://randomuser.me/api/?results=12&inc=name,email,location,cell,dob,picture&nat=gb'
+)
+  .then((response) => response.json())
+  .then((data) => console.log(data.results));
